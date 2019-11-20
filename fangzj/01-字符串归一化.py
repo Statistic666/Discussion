3fc13fb607e220791fa58823a8f4f0af33350bb3
AAAKgold <17621512033@163.com>
2019-11-20 19:50:04 +0800

    修改字符串归一化

--- fangzj/01-字符串归一化.py
+++ fangzj/01-字符串归一化.py
@@ -4,13 +4,13 @@
 #Email:17621512033@163.com
 #Homepage:https://aaakgold.github.io
 #Create Date:2019-11-19 17:11:50
-#Last Modified:2019年11月20日 星期三 19时38分21秒
+#Last Modified:2019年11月20日 星期三 19时49分42秒
 #Description:
 #/
 
 newStr = input("请输入测试字符串：") #按行输入
 newStr_sort = sorted(set(newStr)) #使用set去重后排序
 for ch in newStr_sort:
-    print(ch + str(newStr.count(ch)),end ='')
+    print(ch + str(newStr.count(ch)),end ='') #避免字符之间换行 不好看
 
-# print()
+print() #最后换行 显示清楚
